09ded94bfe56631577f5dd57646aadcfd9954d2c
Zowde <157274222+Zowde@users.noreply.github.com>
2024-01-24 18:24:04 +0200

    Update DamkaBoard.java

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -19,7 +19,7 @@ public static void main (String[] args)
                System.out.print(" ");
               sum++;
                 }
-              System.out.print("* ");
+              System.out.print(" "+"*");
             }
         }
         System.out.println();
