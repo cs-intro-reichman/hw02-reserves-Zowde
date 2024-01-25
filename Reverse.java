18693e09b49428e07ecc7ac0ec238ab5a19133bf
Zowde <157274222+Zowde@users.noreply.github.com>
2024-01-25 13:08:52 +0200

    Update Reverse.java

--- Reverse.java
+++ Reverse.java
@@ -1,9 +1,36 @@
-/**
- * Prints a given string, backward. Then prints the middle character in the string.
- * The program expects to get one command-line argument: A string.
- */
 public class Reverse {
-	public static void main (String[] args){
-		//// Put your code here
-	}
+    public static void main(String[] args)
+    {
+       String word=args[0];
+       int length,i,index;
+       char middle;
+       length=word.length();
+       if(length==1)//if the word comprise from 1 character
+       {
+           middle=word.charAt(0);
+       }
+       else{// finding the middle character
+           index=length/2;
+           if(length%2==0)
+           {
+            middle=word.charAt(index-1);
+           }
+           else
+           {
+
+            middle=word.charAt(index);
+
+            }
+
+       }
+       i=length-1;//goes from backward
+       while(i>=0)
+       {
+        System.out.print(word.charAt(i));;
+        i--;
+       }
+       System.out.println();;
+       System.out.print("The middle character is"+" "+middle);
+
+    }
 }
