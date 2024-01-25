0b382b4e9c8c80c7ef78a1785b357371bbea6f9a
Zowde <157274222+Zowde@users.noreply.github.com>
2024-01-25 13:10:11 +0200

    Update Perfect.java

--- Perfect.java
+++ Perfect.java
@@ -1,8 +1,31 @@
-/**
- *  Gets a command-line argument (int), and chekcs if the given number is perfect.
- */
-public class Perfect {
-	public static void main (String[] args) {
-		//// Put your code here
-	}
+public class perfect {
+    public static void main(String[] args)
+    {
+       int num=Integer.parseInt(args[0]);
+       int sum=0;
+       String divi=" ",divi2;
+       for(int i=1;i<num;i++)
+       {
+        if(num%i==0)
+        {
+            if(sum==0)
+            {
+                divi+=i+" ";
+            }
+            else
+               {
+                divi2="+"+" "+i+" ";
+                divi+=divi2;
+               }
+            
+         sum+=i;
+         
+        }
+    
+       } 
+    if(num==sum)
+    {
+        System.out.print(num+" "+"is a perfect number since"+" "+num+" "+"="+divi);
+    }
+}
 }
